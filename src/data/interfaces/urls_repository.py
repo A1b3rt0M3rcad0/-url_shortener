7bb7e15ae9b270f8d3986b6e5114c7bd89e71b18
alberto <alberto.zimba2013@gmail.com>
2025-02-10 17:41:27 -0300

    config: urls_repository_interface and urls models added

---
+++ src/data/interfaces/urls_repository.py
@@ -0,0 +1,17 @@
+from typing import List, Dict
+from abc import ABC, abstractmethod
+from src.domain.models.urls import Urls
+
+class UrlsRepositoryInterface(ABC):
+
+    @abstractmethod
+    def create(self, user_id:int, link:str, shortened_link:str) -> None:pass
+
+    @abstractmethod
+    def select(self, url_id: int) -> List[Urls]:pass
+
+    @abstractmethod
+    def delete(self, url_id:str) -> None:pass
+
+    @abstractmethod
+    def update(self, url_id:str, update_params:Dict) -> None:pass
---
+++ src/domain/models/urls.py
@@ -0,0 +1,6 @@
+class Urls:
+
+    def __init__(self, user_id:int, link:str, shortened_link:str) -> None:
+        self.user_id = user_id
+        self.link = link
+        self.shortened_link = shortened_link
